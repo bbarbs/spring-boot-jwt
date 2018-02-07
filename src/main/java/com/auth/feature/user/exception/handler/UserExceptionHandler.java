e8d168cfbbf80b8861ed50e94e4c0c2bf46f16d1
Brainold B. Barbero <barberobrainold@gmail.com>
2018-02-07 16:05:54 +0800

    Update UserExceptionHandler.java

--- src/main/java/com/auth/feature/user/exception/handler/UserExceptionHandler.java
+++ src/main/java/com/auth/feature/user/exception/handler/UserExceptionHandler.java
@@ -6,6 +6,7 @@ import com.auth.core.exception.RestExceptionMessage;
 import com.auth.feature.user.exception.EmailExistsException;
 import com.auth.feature.user.exception.EmailNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,9 +28,13 @@ public class UserExceptionHandler {
      * @return
      */
     @ExceptionHandler(EmailExistsException.class)
-    public RestExceptionMessage emailExistsException(RestApiException e) {
-        return new RestExceptionMessage(new Date(), HttpStatus.CONFLICT.value(),
-                HttpStatus.CONFLICT, new ErrorMessage(e.getMessage()));
+    public ResponseEntity<RestExceptionMessage> emailExistsException(RestApiException e) {
+        return new ResponseEntity<>(new RestExceptionMessage(
+                new Date(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT,
+                new ErrorMessage(e.getMessage())
+        ), HttpStatus.CONFLICT);
     }
 
     /**
@@ -39,8 +44,12 @@ public class UserExceptionHandler {
      * @return
      */
     @ExceptionHandler(EmailNotFoundException.class)
-    public RestExceptionMessage emailNotFoundException(RestApiException e) {
-        return new RestExceptionMessage(new Date(), HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND, new ErrorMessage(e.getMessage()));
+    public ResponseEntity<RestExceptionMessage> emailNotFoundException(RestApiException e) {
+        return new ResponseEntity<>(new RestExceptionMessage(
+                new Date(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND,
+                new ErrorMessage(e.getMessage())
+        ), HttpStatus.NOT_FOUND);
     }
 }
